ab17968677504e161c559fbdce5116883592537c
si2022 <pedrohenriquedejesusferreira13@gmail.com>
2022-10-27 08:10:44 -0300

    si2022

--- Segundo Periodo/src/estudodirigido/atividade2.java
+++ Segundo Periodo/src/estudodirigido/atividade2.java
@@ -9,45 +9,73 @@ import java.util.regex.Pattern;
 
 public class atividade2
 {
-    static File arquivo = new File("C:\\Users\\pedro\\IdeaProjects\\si2022\\Segundo Periodo\\src\\estudodirigido\\login.txt");
-    static FileWriter arq;
-
-    static {
-        try {
-            arq = new FileWriter(arquivo,true);
-        } catch (IOException e) {
+    static File arqlogins = new File("C:\\Users\\pedro\\IdeaProjects\\si2022\\Segundo Periodo\\src\\estudodirigido\\login.txt");
+    static File arqsenhas = new File("C:\\Users\\pedro\\IdeaProjects\\si2022\\Segundo Periodo\\src\\estudodirigido\\senha.txt");
+    static FileWriter logins; static
+    {
+        try
+        {
+            logins = new FileWriter(arqlogins,true);
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+    static FileWriter senhas; static
+    {
+        try
+        {
+            senhas = new FileWriter(arqsenhas,true);
+        } catch (IOException e)
+        {
             throw new RuntimeException(e);
         }
     }
 
-    static PrintWriter gravarArq = new PrintWriter(arq);
-    static FileReader lerarquivo;
-
-    static {
-        try {
-            lerarquivo = new FileReader(arquivo);
-        } catch (FileNotFoundException e) {
+    static PrintWriter gravarlogins = new PrintWriter(logins);
+    static PrintWriter gravarsenhas = new PrintWriter(senhas);
+    static FileReader lerlogins; static
+    {
+        try
+        {
+            lerlogins = new FileReader(arqlogins);
+        } catch (FileNotFoundException e)
+        {
             throw new RuntimeException(e);
         }
     }
-
-    static String user;
-    static boolean found;
-    public static void verificarseoarqexist(){
+    static FileReader lersenhas; static
+    {
+        try
+        {
+            lersenhas = new FileReader(arqsenhas);
+        } catch (FileNotFoundException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+    static String user,senha;
+    static boolean found,comfirme;
+/*    public static void verificarseoarqexist(){
 
         try
         {
-            if (!arquivo.exists())
+            if (!arqlogins.exists())
             {
-                arquivo.createNewFile();
+                arqlogins.createNewFile();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
+    }*/
     public static void MensagemdeErro(){
         JOptionPane.showMessageDialog(null, """
                          usuario invalido
+                     atente- se aos requisitos""", "erro!!",JOptionPane.INFORMATION_MESSAGE);
+
+    }    public static void MensagemdeErro2(){
+        JOptionPane.showMessageDialog(null, """
+                         senha invalida
                      atente- se aos requisitos""", "erro!!",JOptionPane.INFORMATION_MESSAGE);
 
     }
@@ -65,23 +93,22 @@ public class atividade2
              VerificaodeLogin(user);
          }else MensagemdeErro();
      }
-     public static void VerificaodeLogin(String user)
+     public static boolean VerificaodeLogin(String user)
      {
              Pattern pattern = Pattern.compile("^[A-z_].(?!.*[\\s]).(?!.*[\\p{Punct}&&[^_]]).(?=\\w)");
              Matcher matcher = pattern.matcher(user);
              found = matcher.find();
+             boolean x = false;
 
-             if (!getLerarquivo()&&found)
-             {
-                 gravarArq.println(user);
-                 gravarArq.flush();
-                 senha();
-             } else MensagemdeErro();
+             if (!getLerarquivo1()&&found)
+             {return comfirme = true;
+             } else {MensagemdeErro();}
+         return x;
      }
 
-    public static boolean getLerarquivo() {
+    public static boolean getLerarquivo1() {
         boolean y=false;
-        Scanner ler = new Scanner(lerarquivo);
+        Scanner ler = new Scanner(lerlogins);
         while (ler.hasNext())
         {
             String x = ler.nextLine();
@@ -91,14 +118,30 @@ public class atividade2
                 JOptionPane.showMessageDialog(null, """
                         esse usuario ja existe!
                         digite um novo nome de usuario""","erro",JOptionPane.INFORMATION_MESSAGE);
+                CadastrodeLogin();
 
             }
         }
         return y;
+    }    public static boolean getLerarquivo2() {
+        boolean y=false;
+
+            if (senha.equals(user))
+            {
+                y=true;
+                JOptionPane.showMessageDialog(null, """
+                        senha é igual usuario!
+                        digite uma senha diferente""","erro",JOptionPane.INFORMATION_MESSAGE);
+
+            }
+
+        return y;
     }
-    public static void senha(){
-        JOptionPane.showInputDialog(null, """
-            sua seha deve conter:
+    public static void CadastrarSenha(){
+
+        if (comfirme)
+        {        senha = JOptionPane.showInputDialog(null, """
+            sua senha deve conter:
                 -minimo de 8 digitos e maximo de 16 digitos
                 -deve incluir pelo menos um numero e 2 letras
                 -deve incluir pelo menos uma letra maiuscula e uma minuscula
@@ -108,14 +151,37 @@ public class atividade2
                 
                 digite sua senha:
                 """, "cadastrar senha",JOptionPane.QUESTION_MESSAGE);
+            VerificaodeSenha(senha);
+        }else MensagemdeErro2();}
+
+
+    private static void VerificaodeSenha(String senha)
+    {
+        Pattern pattern = Pattern.compile("(?=.+[a-z]).(?=.+[A-Z]).(?=.+[0-9]).(?!.*([1-9]+)\1{1,}).(?!.*[\s]).{4,12}");
+        Matcher matcher = pattern.matcher(senha);
+        found = matcher.find();
+
+
+        if (senha.length() >=8 && senha.length()<= 16 && !getLerarquivo2()&&found)
+        {
+            setGravarLoginseSenhas();
+        } else MensagemdeErro2();
+    }
+    public static void setGravarLoginseSenhas(){
+        gravarlogins.println(user);
+        gravarlogins.flush();
+        gravarsenhas.println(senha);
+        gravarsenhas.flush();
+        JOptionPane.showMessageDialog(null,"usuario cadastrado com sucesso!!!!!!!","cadastrado com sucesso!!!",JOptionPane.INFORMATION_MESSAGE);
+        comfirme=false;
     }
 
 
-
-
     public static void main(String[] args) throws IOException {
-             while (true) {
-                 verificarseoarqexist();
-                 CadastrodeLogin();
-                 if (found){senha();}
-         }}}
+        while (true) {
+            CadastrodeLogin();
+            while (comfirme) {
+                CadastrarSenha();
+            }
+        }
+    }}
